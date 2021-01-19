583bc93cc6f00e6aaf7db6d34f41107f4702ea87
jaepil-choi <chljeffreyz@gmail.com>
2021-01-19 14:10:33 +0900

    Solve text processing regex

--- text_processing.py
+++ text_processing.py
@@ -6,7 +6,7 @@
 NLP에서 흔히하는 전처리는 소문자 변환, 앞뒤 필요없는 띄어쓰기를 제거하는 등의 텍스트 정규화 (text normalization)입니다. 
 이번 숙제에서는 텍스트 처리 방법을 파이썬으로 배워보겠습니다. 
 """
-
+import re
 
 def normalize(input_string):
     """
@@ -33,7 +33,9 @@ def normalize(input_string):
              >>> tp.normalize(input_string2)
              'extra space'
     """
-    normalized_string = None
+    normalized_string = input_string.lower().strip()
+    normalized_string = re.sub(r'\s{2,}', ' ', normalized_string)
+
     return normalized_string
 
 
@@ -58,5 +60,5 @@ def no_vowels(input_string):
             >>> tp.normalize(input_string2)
             ''W lv Pythn!'
     """
-    no_vowel_string = None
+    no_vowel_string = re.sub(r'[aeiou]', '', input_string)
     return no_vowel_string
